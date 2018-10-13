d65ba35ea9cad88b30e40afa65fc96fa773d966f
Maria <mmoskalenkouo@gmail.com>
2018-10-13 16:43:44 +0300

    [swup]minor fixes in js

--- test_addon/index.js
+++ test_addon/index.js
@@ -2,33 +2,31 @@ const http = require('http');
 const port = 3000;
 const url = require('url');
 const addon = require('sqlite3_addon');
-const parser = require("./parser");
 
-var queryCreate = "CREATE TABLE IF NOT EXISTS tickets(id UNIQUE, name, status);";
-var querysToInsert = [];
-var querySelect = "SELECT * from tickets";
-
-function formQueryToInsertAndExecute(instance, fileName, outQuery) {
-	parser.parseFileFormQuery(fileName, outQuery);
-	outQuery.forEach(function(item, i) {
-		var result = instance.executeQuery(item);
-		console.log( "==>" + result );
-	});
-}
+var queryCreate = 	"CREATE TABLE IF NOT EXISTS COMPANY(ID INT PRIMARY KEY NOT NULL, NAME TEXT NOT NULL,\
+					 AGE INT NOT NULL, ADDRESS CHAR(50), SALARY REAL );";
+var querysToInsert = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)\
+					 VALUES (1, 'Paul', 32, 'California', 20000.00 );\
+					 INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)\
+					 VALUES (2, 'Allen', 25, 'Texas', 15000.00 );\
+					 INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)\
+					 VALUES (3, 'Teddy', 23, 'Norway', 20000.00 );\
+					 INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)\
+					 VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 );";
+var querySelect = "SELECT * from COMPANY";
 
 const requestHandler = (request, response) => {
 	if (request.url != '/favicon.ico') {
 		console.log("call new " + request.url);
 		var obj = new addon.CommandQueue();
-		var res = obj.openDB("myDatabase.db");
+		var res = obj.openDB("test.db");
 		console.log("==> 1 " + res);
 		res = obj.executeQuery(queryCreate);
 		console.log("==> 2 " + res);
-		formQueryToInsertAndExecute(obj, "output.html", querysToInsert);
-		formQueryToInsertAndExecute(obj, "output2.html", querysToInsert);
-		formQueryToInsertAndExecute(obj, "output3.html", querysToInsert);
-		res = obj.executeQuery(querySelect);
+		res = obj.executeQuery(querysToInsert);
 		console.log("==> 3 " + res);
+		res = obj.executeQuery(querySelect);
+		console.log("==> 4 " + res);
 		obj.closeDB();
 		response.writeHead(200, {'Content-Type': 'text/javascript'});
 		response.write(res.toString() + " ");
